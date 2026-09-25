1d4bcfbde0ce966ddc69b1f8b111762ce262bff8
pmakpah-ops <pmakpah@gmail.com>
2026-09-25 07:05:29 +0100

    fix: ring no longer blocks tap [skip ci]

--- lib/widgets/deck_light_ring.dart
+++ lib/widgets/deck_light_ring.dart
@@ -1,34 +1,21 @@
 import 'package:flutter/material.dart';
-import 'dart:math';
-
-class DeckLightRing extends StatefulWidget {
-  final bool isPlaying;
-  final bool isLive;
-  final double audioLevel;
-  final Widget child;
-  final double size;
-  const DeckLightRing({super.key, required this.isPlaying, required this.isLive, this.audioLevel = 0, required this.child, this.size = 210});
-  @override
-  State<DeckLightRing> createState() => _DeckLightRingState();
+class DeckLightRing extends StatefulWidget{
+  final bool isLive,isPlaying;final Widget child;
+  const DeckLightRing({super.key,required this.isLive,required this.isPlaying,required this.child});
+  @override State<DeckLightRing> createState()=>_R();
 }
-
-class _DeckLightRingState extends State<DeckLightRing> with SingleTickerProviderStateMixin {
-  late AnimationController _ctrl;
-  @override
-  void initState() { super.initState(); _ctrl = AnimationController(vsync: this, duration: const Duration(seconds: 4))..repeat(); }
-  @override
-  void dispose(){ _ctrl.dispose(); super.dispose(); }
-  @override
-  Widget build(BuildContext context) {
-    if (!widget.isPlaying) {
-      return Container(width: widget.size, height: widget.size, decoration: BoxDecoration(shape: BoxShape.circle, border: Border.all(color: Colors.grey.shade900, width: 6)), child: Center(child: widget.child));
-    }
-    return AnimatedBuilder(animation: _ctrl, builder: (_, __) {
-      double t = _ctrl.value; double level = widget.audioLevel.clamp(0,1);
-      Color c1 = Color.lerp(Colors.redAccent, Colors.greenAccent, (sin(t*2*pi)+1)/2)!;
-      Color c2 = Color.lerp(Colors.greenAccent, Colors.cyanAccent, (cos(t*2*pi)+1)/2)!;
-      Color c3 = Color.lerp(Colors.blueAccent, Colors.redAccent, (sin(t*pi)+1)/2)!;
-      return Container(width: widget.size, height: widget.size, decoration: BoxDecoration(shape: BoxShape.circle, gradient: SweepGradient(colors: [c1,c2,c3,c1])), child: Padding(padding: EdgeInsets.all(5 + level*3), child: Container(decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.black, boxShadow: widget.isLive ? [BoxShadow(color: c2.withOpacity(0.5 + level*0.5), blurRadius: 12 + level*18)] : []), child: Center(child: widget.child))));
-    });
+class _R extends State<DeckLightRing> with SingleTickerProviderStateMixin{
+  late AnimationController c;
+  @override void initState(){super.initState();c=AnimationController(vsync:this,duration:Duration(milliseconds:1200))..repeat();}
+  @override void dispose(){c.dispose();super.dispose();}
+  @override Widget build(BuildContext context){
+    return Stack(alignment:Alignment.center,children:[
+      widget.child,
+      if(widget.isLive&&widget.isPlaying) IgnorePointer(
+        child: AnimatedBuilder(animation:c,builder:(ctx,_){
+          return Container(width:220,height:220,decoration:BoxDecoration(shape:BoxShape.circle,border:Border.all(color:Color(0xFFCEBBFF).withOpacity(0.5+0.5*c.value),width:3),boxShadow:[BoxShadow(color:Color(0xFFCEBBFF).withOpacity(0.3),blurRadius:12+8*c.value,spreadRadius:2)]) );
+        }),
+      ),
+    ]);
   }
 }
